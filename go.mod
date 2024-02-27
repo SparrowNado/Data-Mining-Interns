ee351fd48b5211f99b5ea881e32d48d2b87d9514
Christian Gabriel D. Ramoso <123578310+ChristianGabrielDRamoso@users.noreply.github.com>
2024-02-27 18:09:15 +0800

    added  first working version

---
+++ go.mod
@@ -0,0 +1,61 @@
+module playground
+
+go 1.22.0
+
+require (
+	github.com/BurntSushi/toml v1.3.2 // indirect
+	github.com/CycloneDX/cyclonedx-go v0.8.0 // indirect
+	github.com/Microsoft/go-winio v0.6.1 // indirect
+	github.com/anchore/go-struct-converter v0.0.0-20221118182256-c68fdcfa2092 // indirect
+	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
+	github.com/carbonetes/diggity v1.10.0 // indirect
+	github.com/charmbracelet/bubbles v0.17.1 // indirect
+	github.com/charmbracelet/bubbletea v0.25.0 // indirect
+	github.com/charmbracelet/lipgloss v0.9.1 // indirect
+	github.com/containerd/console v1.0.4-0.20230313162750-1ae8d489ac81 // indirect
+	github.com/containerd/stargz-snapshotter/estargz v0.14.3 // indirect
+	github.com/distribution/reference v0.5.0 // indirect
+	github.com/docker/cli v24.0.0+incompatible // indirect
+	github.com/docker/distribution v2.8.3+incompatible // indirect
+	github.com/docker/docker v24.0.0+incompatible // indirect
+	github.com/docker/docker-credential-helpers v0.7.0 // indirect
+	github.com/docker/go-connections v0.4.0 // indirect
+	github.com/docker/go-units v0.5.0 // indirect
+	github.com/facebookincubator/nvdtools v0.1.5 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/google/go-containerregistry v0.17.0 // indirect
+	github.com/google/uuid v1.5.0 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/klauspost/compress v1.16.5 // indirect
+	github.com/knqyf263/go-rpmdb v0.0.0-20231206071317-a8af76a6220f // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-isatty v0.0.18 // indirect
+	github.com/mattn/go-localereader v0.0.1 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/muesli/ansi v0.0.0-20211018074035-2e021307bc4b // indirect
+	github.com/muesli/cancelreader v0.2.2 // indirect
+	github.com/muesli/reflow v0.3.0 // indirect
+	github.com/muesli/termenv v0.15.2 // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/opencontainers/image-spec v1.1.0-rc3 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	github.com/spdx/tools-golang v0.5.3 // indirect
+	github.com/spf13/cobra v1.8.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/t-tomalak/logrus-easy-formatter v0.0.0-20190827215021-c074f06c5816 // indirect
+	github.com/vbatts/tar-split v0.11.3 // indirect
+	gitlab.com/jhumel/grove v1.0.3 // indirect
+	golang.org/x/mod v0.14.0 // indirect
+	golang.org/x/net v0.15.0 // indirect
+	golang.org/x/sync v0.3.0 // indirect
+	golang.org/x/sys v0.12.0 // indirect
+	golang.org/x/term v0.12.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	golang.org/x/tools v0.13.0 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,218 @@
+package main
+
+import (
+	"encoding/json"
+	"encoding/xml"
+	"io"
+	"os"
+	"path/filepath"
+)
+
+type Vulnerability struct {
+	CVE         string                 `json:"cve,omitempty"`
+	Package     string                 `json:"package,omitempty"`
+	Metadata    map[string]interface{} `json:"metadata,omitempty"`
+	Identifiers []string               `json:"identifiers,omitempty"`
+}
+
+func main() {
+	// Process the JSONS directory recursively
+	err := processDirectory("JSONS")
+	if err != nil {
+		panic(err)
+	}
+}
+
+func processDirectory(dirPath string) error {
+	// Open the directory
+	dirEntries, err := os.ReadDir(dirPath)
+	if err != nil {
+		return err
+	}
+
+	// Process each entry in the directory
+	for _, entry := range dirEntries {
+		fullPath := filepath.Join(dirPath, entry.Name())
+		if entry.IsDir() {
+			// If it's a directory, recursively process it
+			err := processDirectory(fullPath)
+			if err != nil {
+				return err
+			}
+		} else {
+			// If it's a file, check if it's a .json file
+			if filepath.Ext(entry.Name()) == ".json" {
+				err := processJSON(fullPath)
+				if err != nil {
+					return err
+				}
+			}
+		}
+	}
+	return nil
+}
+func processJSON(filePath string) error {
+	// Define SUSE struct locally within the function scope
+	type SUSE struct {
+		XMLName        xml.Name `xml:"oval_definitions" json:"oval_definitions,omitempty"`
+		SchemaLocation string   `xml:"schemaLocation,attr" json:"schemalocation,omitempty"`
+		Xmlns          string   `xml:"xmlns,attr" json:"xmlns,omitempty"`
+		Xsi            string   `xml:"xsi,attr" json:"xsi,omitempty"`
+		Oval           string   `xml:"oval,attr" json:"oval,omitempty"`
+		OvalDef        string   `xml:"oval-def,attr" json:"oval-def,omitempty"`
+		Generator      struct {
+			ProductName   string `xml:"product_name"`
+			SchemaVersion string `xml:"schema_version"`
+			Timestamp     string `xml:"timestamp"`
+		} `xml:"generator" json:"generator,omitempty"`
+		Definitions struct {
+			Definition []struct {
+				ID       string `xml:"id,attr" json:"id,omitempty"`
+				Version  string `xml:"version,attr" json:"version,omitempty"`
+				Class    string `xml:"class,attr" json:"class,omitempty"`
+				Metadata struct {
+					Title    string `xml:"title"`
+					Affected struct {
+						Family   string `xml:"family,attr" json:"family,omitempty"`
+						Platform string `xml:"platform"`
+					} `xml:"affected" json:"affected,omitempty"`
+					Reference struct {
+						RefID  string `xml:"ref_id,attr" json:"ref_id,omitempty"`
+						RefURL string `xml:"ref_url,attr" json:"ref_url,omitempty"`
+						Source string `xml:"source,attr" json:"source,omitempty"`
+					} `xml:"reference" json:"reference,omitempty"`
+					Description string `xml:"description"`
+				} `xml:"metadata" json:"metadata,omitempty"`
+				Criteria struct {
+					Operator  string `xml:"operator,attr" json:"operator,omitempty"`
+					Criterion []struct {
+						TestRef string `xml:"test_ref,attr" json:"test_ref,omitempty"`
+						Comment string `xml:"comment,attr" json:"comment,omitempty"`
+					} `xml:"criterion" json:"criterion,omitempty"`
+					Criteria []struct {
+						Operator  string `xml:"operator,attr" json:"operator,omitempty"`
+						Criterion []struct {
+							TestRef string `xml:"test_ref,attr" json:"test_ref,omitempty"`
+							Comment string `xml:"comment,attr" json:"comment,omitempty"`
+						} `xml:"criterion" json:"criterion,omitempty"`
+						Criteria struct {
+							Operator  string `xml:"operator,attr" json:"operator,omitempty"`
+							Criterion []struct {
+								TestRef string `xml:"test_ref,attr" json:"test_ref,omitempty"`
+								Comment string `xml:"comment,attr" json:"comment,omitempty"`
+							} `xml:"criterion" json:"criterion,omitempty"`
+						} `xml:"criteria" json:"criteria,omitempty"`
+					} `xml:"criteria" json:"criteria,omitempty"`
+				} `xml:"criteria" json:"criteria,omitempty"`
+			} `xml:"definition" json:"definition,omitempty"`
+		} `xml:"definitions" json:"definitions,omitempty"`
+		Tests struct {
+			RpminfoTest []struct {
+				ID      string `xml:"id,attr" json:"id,omitempty"`
+				Version string `xml:"version,attr" json:"version,omitempty"`
+				Comment string `xml:"comment,attr" json:"comment,omitempty"`
+				Check   string `xml:"check,attr" json:"check,omitempty"`
+				Xmlns   string `xml:"xmlns,attr" json:"xmlns,omitempty"`
+				Object  struct {
+					ObjectRef string `xml:"object_ref,attr" json:"object_ref,omitempty"`
+				} `xml:"object" json:"object,omitempty"`
+				State struct {
+					StateRef string `xml:"state_ref,attr" json:"state_ref,omitempty"`
+				} `xml:"state" json:"state,omitempty"`
+			} `xml:"rpminfo_test" json:"rpminfo_test,omitempty"`
+		} `xml:"tests" json:"tests,omitempty"`
+		Objects struct {
+			RpminfoObject []struct {
+				ID      string `xml:"id,attr" json:"id,omitempty"`
+				Version string `xml:"version,attr" json:"version,omitempty"`
+				Xmlns   string `xml:"xmlns,attr" json:"xmlns,omitempty"`
+				Name    string `xml:"name"`
+			} `xml:"rpminfo_object" json:"rpminfo_object,omitempty"`
+		} `xml:"objects" json:"objects,omitempty"`
+		States struct {
+			RpminfoState []struct {
+				ID          string `xml:"id,attr" json:"id,omitempty"`
+				AttrVersion string `xml:"version,attr" json:"version,omitempty"`
+				Xmlns       string `xml:"xmlns,attr" json:"xmlns,omitempty"`
+				Version     struct {
+					Operation string `xml:"operation,attr" json:"operation,omitempty"`
+				} `xml:"version" json:"version,omitempty"`
+				Evr struct {
+					Datatype  string `xml:"datatype,attr" json:"datatype,omitempty"`
+					Operation string `xml:"operation,attr" json:"operation,omitempty"`
+				} `xml:"evr" json:"evr,omitempty"`
+			} `xml:"rpminfo_state" json:"rpminfo_state,omitempty"`
+		} `xml:"states" json:"states,omitempty"`
+	}
+
+	// Open the JSON file
+	file, err := os.Open(filePath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	// Read the JSON file into a byte slice
+	data, err := io.ReadAll(file)
+	if err != nil {
+		return err
+	}
+
+	var rawData SUSE
+
+	// Unmarshal the JSON data into the SUSE struct
+	err = json.Unmarshal([]byte(data), &rawData)
+	if err != nil {
+		return err
+	}
+
+	vulns := make(map[string]Vulnerability)
+
+	// Process the vulnerability data
+	for _, d := range rawData.Definitions.Definition {
+		var vuln Vulnerability
+		if existingVuln, ok := vulns[d.ID]; ok {
+			// If vulnerability already exists, use existing data
+			vuln = existingVuln
+		}
+		vuln.CVE = d.Metadata.Title
+		vuln.Package = d.Metadata.Affected.Platform
+		vuln.Identifiers = append(vuln.Identifiers, d.ID)
+		metadata := make(map[string]interface{})
+		metadata[filePath] = d.Metadata
+		vuln.Metadata = metadata
+		vulns[d.ID] = vuln
+	}
+
+	// Determine the output directory path
+	outputDir := filepath.Join("OUTPUT", filepath.Dir(filePath))
+	err = os.MkdirAll(outputDir, 0755)
+	if err != nil {
+		return err
+	}
+
+	// Extract the filename
+	outputFileName := filepath.Base(filePath)
+
+	// Construct the output file path
+	outputFile := filepath.Join(outputDir, outputFileName)
+
+	// Write the vulns to the output JSON file
+	vulnsFile, err := os.Create(outputFile)
+	if err != nil {
+		return err
+	}
+	defer vulnsFile.Close()
+
+	vulnsData, err := json.MarshalIndent(vulns, "", "    ")
+	if err != nil {
+		return err
+	}
+
+	_, err = vulnsFile.Write(vulnsData)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
